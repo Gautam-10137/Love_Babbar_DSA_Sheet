3429e5d9f4def65a471da3bda65ad694abd8ef6d
Gautam-10137 <pahwagautam786@gmail.com>
2025-02-19 23:39:48 +0530

    21_binary_tree --doing

---
+++ Binary Tree/21_min_swaps.cpp
@@ -0,0 +1,27 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    int minSwaps(vector<int> &arr)
+    {
+        // Write your code here
+    }
+};
