02027c8899f94c042de35ca521517e03e409e958
Gautam-10137 <pahwagautam786@gmail.com>
2025-02-27 22:59:28 +0530

    4_bit_manipulation --doing

---
+++ Bit_Manipulation/4_count_set_bits.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+  public:
+    // n: input to count the number of set bits
+    //Function to return sum of count of set bits in the integers from 1 to n.
+    int countSetBits(int n)
+    {
+        // Your logic here
+    }
+};
