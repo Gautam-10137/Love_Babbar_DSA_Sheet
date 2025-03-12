cfb0718456203976fa5be7148e2398fed63bdf0f
Gautam-10137 <pahwagautam786@gmail.com>
2025-03-12 18:54:40 +0530

    4_bit_manipulation --done

--- Bit_Manipulation/4_count_set_bits.cpp
+++ Bit_Manipulation/4_count_set_bits.cpp
@@ -10,16 +10,32 @@ class Solution {
     {
 
         // brute-force approach
-        int count=0;
-        int i=n;
-        while(i>0){
-          int num=i;
-          while(num){
-            if(num & 1) count++;
-            num=num>>1;
-          }
-          i--;
-        }
-        return count;
+        // int count=0;
+        // int i=n;
+        // while(i>0){
+        //   int num=i;
+        //   while(num){
+        //     if(num & 1) count++;
+        //     num=num>>1;
+        //   }
+        //   i--;
+        // }
+        // return count;
+
+        // log(n) solution
+        if(n==0) return 0;
+        // Find the largest power of 2 that is <=n
+        int x=log2(n);
+        int power_of_2= 1<<x;   // equivalent to 2^x
+        
+        // count of set bits from 0 to 2^x-1
+        int bits_up_to_2x= x*(power_of_2>>1); // x*(2^(x-1))
+        
+        // count MSB Contribution from 2^x to n
+        int msb_from_2x_to_n= n-power_of_2+1;
+        
+        // recur for remaining numbers after 2^x
+        int remaining= countSetBits(n - power_of_2);
+        return bits_up_to_2x + msb_from_2x_to_n + remaining;
     }
 };
