d07ac55e6958944e569071422acff052bbbfc78b
Gautam-10137 <pahwagautam786@gmail.com>
2025-05-07 23:40:18 +0530

    15_bst --doing

---
+++ Binary_Search_Tree/15_median_in_bst.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+class Solution {
+    public:
+    
+    float findMedian(struct Node *root) {
+        // Code here
+        
+    }
+  };
