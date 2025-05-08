9b9fb9e058c926d78a175f479a0cacfe943797d2
Gautam-10137 <pahwagautam786@gmail.com>
2025-05-08 23:07:08 +0530

    15_bst --done

--- Binary_Search_Tree/15_median_in_bst.cpp
+++ Binary_Search_Tree/15_median_in_bst.cpp
@@ -17,8 +17,41 @@ struct Node {
 class Solution {
     public:
     
+    int countNodes(struct Node* root){
+        if(!root) return 0;
+        
+        return 1+countNodes(root->left)+countNodes(root->right);
+    }
+    
+    void getNodes(struct Node* root,int& idx,int& num){
+        if(!root) return;
+        if(idx<0) return;
+        getNodes(root->left,idx,num);
+        idx--;
+        if(idx==0){
+            num=root->data;
+            return;
+        }
+        getNodes(root->right,idx,num);
+    }
     float findMedian(struct Node *root) {
         // Code here
         
+        int n=countNodes(root);
+        
+        
+        int idx=n/2;
+        int num1=0;
+        getNodes(root,idx,num1);
+        int num2=0;
+        idx=n/2+1;
+        getNodes(root,idx,num2);
+        
+        if(n%2==0){
+            return (num1+num2)/2.0;
+        }
+        else{
+            return num2*1.0;
+        }
     }
   };
