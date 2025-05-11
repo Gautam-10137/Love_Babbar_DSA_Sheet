c97bec902b3d234a98e8b6b5b2864b125cb3056b
Gautam-10137 <pahwagautam786@gmail.com>
2025-05-11 23:42:46 +0530

    5_array --done

---
+++ Array/5_move_negative_to_start.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+  public:
+  // using Dutch National Flag Algo
+  vector<int> move(vector<int>& arr){
+    int n=arr.size();
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]<0) cnt++;
+    }
+    int i=0;
+    for(int j=0;j<cnt;j++){
+        while(i<n && arr[i]>0) i++;
+        
+        swap(arr[i],arr[j]);
+        i++;
+    }
+
+    return arr;
+  }
+};
+
