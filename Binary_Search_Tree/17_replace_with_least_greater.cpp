91b50a59a9519ba894b3ef47998134dccb7f1459
Gautam-10137 <pahwagautam786@gmail.com>
2025-05-27 23:50:53 +0530

    17_bst --done

--- Binary_Search_Tree/17_replace_with_least_greater.cpp
+++ Binary_Search_Tree/17_replace_with_least_greater.cpp
@@ -6,8 +6,34 @@ using namespace std;
 class Solution {
     public:
     
+     struct Node{
+        int data;
+        Node* left;
+        Node* right;
+        Node(int val): data(val),left(NULL),right(NULL){}
+    };
+    
+    Node* insertNode(Node* root,int key,Node*&successor){
+        if(!root) return new Node(key);
+        if(root->data > key){
+            successor=root;
+            root->left=insertNode(root->left,key,successor);
+        }
+        else{
+            root->right=insertNode(root->right,key,successor);
+        }
+        return root;
+    }
+    
     vector<int> findLeastGreater(vector<int>& arr, int n) {
+        vector<int> result(n,-1);
         
-        
+        Node* root=NULL;
+        for(int i=n-1;i>=0;i--){
+            Node* successor=NULL;
+            root=insertNode(root,arr[i],successor);
+            if(successor) result[i]=successor->data;
+        }
+        return result;
     }
   };
