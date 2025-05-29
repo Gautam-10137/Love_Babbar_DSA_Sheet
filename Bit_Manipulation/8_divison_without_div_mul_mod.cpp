151ac39b36eb7978fefffc6d18f2a39d0f0fbfff
Gautam-10137 <pahwagautam786@gmail.com>
2025-05-29 22:34:17 +0530

    8_bst --doing

---
+++ Bit_Manipulation/8_divison_without_div_mul_mod.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+  public:
+  
+  long long divide(long long dividend, long long divisor) {
+        // code here
+        
+  }
+};
