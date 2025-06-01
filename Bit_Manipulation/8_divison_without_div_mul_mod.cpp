1a94429a5a18c9d63fb5691c0cecb57313ecc4d1
Gautam-10137 <pahwagautam786@gmail.com>
2025-06-01 19:25:57 +0530

    8_bit_manipulatkion --done

--- Bit_Manipulation/8_divison_without_div_mul_mod.cpp
+++ Bit_Manipulation/8_divison_without_div_mul_mod.cpp
@@ -6,7 +6,22 @@ class Solution {
   public:
   
   long long divide(long long dividend, long long divisor) {
-        // code here
         
-  }
+        if(dividend==INT_MIN && divisor==-1) return INT_MAX;
+        int sign= (dividend<0)^(divisor<0)?-1:1;
+        
+        dividend=abs(dividend);
+        divisor=abs(divisor);
+        int qoutient=0;
+        
+        for(int i=31;i>=0;i--){
+            if(dividend>=(divisor<<i)*1LL){
+                dividend-= (divisor<<i)*1LL;
+                qoutient|=1LL<<i;
+            }    
+          
+            
+        }
+        return qoutient*sign;
+    }
 };
