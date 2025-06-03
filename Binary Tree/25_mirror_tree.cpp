5a77fabc29d750c93b5a86b0ba8dada3be2df4a0
Gautam-10137 <pahwagautam786@gmail.com>
2025-06-03 23:59:53 +0530

    25_binary_tee --doing

---
+++ Binary Tree/25_mirror_tree.cpp
@@ -0,0 +1,23 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+class Solution {
+public:
+   
+};
+
