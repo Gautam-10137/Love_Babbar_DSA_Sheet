69dbb991d4f017fe8cf642b842c10d7fd244a919
Gautam-10137 <pahwagautam786@gmail.com>
2025-06-04 22:38:33 +0530

    25_binary_tree --done

--- Binary Tree/25_mirror_tree.cpp
+++ Binary Tree/25_mirror_tree.cpp
@@ -15,9 +15,39 @@ class Node {
     }
 };
 
+// using stack and queue
 
 class Solution {
 public:
-   
+       int checkMirrorTree(int n, int e, int A[], int B[]) {
+        // code here
+        unordered_map<int,stack<int>> treeA;
+        unordered_map<int,queue<int>> treeB;
+        
+        for(int i=0;i<2*e;i+=2){
+            treeA[A[i]].push(A[i+1]);
+        }
+        
+        for(int i=0;i<2*e;i+=2){
+            treeB[B[i]].push(B[i+1]);
+        }
+        
+        for(auto it : treeA){
+            int node=it.first;
+            while(!it.second.empty() && !treeB[node].empty()){
+                if( treeA[node].top()!=treeB[node].front()){
+                    return 0;
+                }
+                treeA[node].pop();
+                treeB[node].pop();
+            }
+            
+            if(!treeA[node].empty() || !treeB[node].empty())
+              return 0;
+            
+        }
+        return 1;
+            
+    }
 };
 
