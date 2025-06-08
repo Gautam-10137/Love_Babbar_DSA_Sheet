dc9dc66a27b7fc234416a3c0cb0ba605049f10fe
Gautam-10137 <pahwagautam786@gmail.com>
2025-06-08 23:52:40 +0530

    01_linked_list --doing

---
+++ Linked_List/01_reverse_list.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+
+class Solution {
+  public:
+  
+    Node* reverseList(struct Node* head) {
+        // code here
+        
+    }
+};
