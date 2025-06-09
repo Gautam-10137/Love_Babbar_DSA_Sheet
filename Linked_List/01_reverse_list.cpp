eabf8a2f9c68404444b4d75ee6721d70f490b9d7
Gautam-10137 <pahwagautam786@gmail.com>
2025-06-09 22:34:36 +0530

    01_linked_list --recursive --doing

--- Linked_List/01_reverse_list.cpp
+++ Linked_List/01_reverse_list.cpp
@@ -11,6 +11,17 @@ struct Node
 class Solution {
   public:
   // recursive way
+  Node* reverse(struct Node* head){
+    if(!head || !head->next) return head;
+    
+    Node* curr=head;
+    Node* next=curr->next;
+    Node* newHead=reverse(next);
+
+    next->next=curr;
+    return newHead;
+
+  }
 
   // iterative way
     Node* reverseList(struct Node* head) {
