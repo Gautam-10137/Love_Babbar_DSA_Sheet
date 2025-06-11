e818a4e3ad3ed8bbbe4ab1641049fa26d88ee826
Gautam-10137 <pahwagautam786@gmail.com>
2025-06-11 23:42:20 +0530

    04_linked_list --doing

---
+++ Linked_List/04_delete_loop.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+
+class Solution {
+  public:
+
+    // using slow and fast pointers  
+    bool deleteLoop(Node* head) {
+        
+    }
+};
