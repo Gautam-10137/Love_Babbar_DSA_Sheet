17e66d0b559582b4cdb45d1b45878260c9f36873
Gautam-10137 <pahwagautam786@gmail.com>
2025-06-14 22:01:17 +0530

    04_linked_list --done

--- Linked_List/04_delete_loop.cpp
+++ Linked_List/04_delete_loop.cpp
@@ -12,7 +12,32 @@ class Solution {
   public:
 
     // using slow and fast pointers  
-    bool deleteLoop(Node* head) {
+    void removeLoop(Node* head) {
+      
+        Node* slow=head;
+        Node* fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast) break;
+        }
+        if(!fast || !fast->next) return;
+        fast=head;
+
+        // if both slow and fast ast head
+        if(slow==fast){
+            while(slow->next!=fast) slow=slow->next;
+            
+        }
+        // else 
+        else{
+           while(slow->next!=fast->next){
+            slow=slow->next;
+            fast=fast->next;
+           } 
+        }
         
+        slow->next=NULL;
+        return;
     }
 };
